63114345607e99b0c143420b2abfefaeec0e9670
jeromnhsu-daddy <jeromnhsu@gmail.com>
2026-04-10 23:16:46 +0800

    Support 4 gen
    support 4 gen, 4 areas, cross gen evolution,  card exchange

--- docs/assets/layout-grid-DjYwsRAb.js
+++ docs/assets/layout-grid-DjYwsRAb.js
@@ -1,4 +1,4 @@
-import{c as t}from"./index-BVjF559S.js";/**
+import{c as t}from"./index-CcYb80DV.js";/**
  * @license lucide-react v0.546.0 - ISC
  *
  * This source code is licensed under the ISC license.
